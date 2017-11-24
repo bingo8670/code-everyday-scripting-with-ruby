dd07d225ec88ff0509f288de2f0b8db28a3f6a59
bingo8670 <307286130@qq.com>
2017-11-24 23:09:36 +0800

    blank

--- churn/snapshots/churn.v4.rb
+++ churn/snapshots/churn.v4.rb
@@ -1,6 +1,6 @@
 #---
 # Excerpted from "Everyday Scripting in Ruby"
-# We make no guarantees that this code is fit for any purpose. 
+# We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/bmsft for more book information.
 #---
 def month_before(a_time)
@@ -21,13 +21,14 @@ def asterisks_for(an_integer)
 end
 
 
+
 if $0 == __FILE__
   subsystem_names = ['audit', 'fulfillment', 'persistence',
                      'ui', 'util', 'inventory']
   start_date = month_before(Time.now)
 
   puts header(start_date)
-  subsystem_names.each do | name |          
+  subsystem_names.each do | name |
     puts subsystem_line(name, change_count_for(name))
   end
 end
